d1c7fba5ae1dc93006850139782439f970d4d1de
ATA AVCI <129445221+ataavci@users.noreply.github.com>
2024-09-22 06:14:59 +0300

    tarih eksik

--- server.js
+++ server.js
@@ -247,97 +247,128 @@ app.post("/sefer-kaydet", (req, res) => {
             ? ecaConsumption
             : 0;
 
-          const fuelQuery = `SELECT CO2eqWtT,Cf_CO2_ggFuel,Cf_CO2eq_TtW, CO2eqTTW,CO2eqWtT, LCV FROM fuel_data WHERE Pathway_Name = ?`;
+            const fuelQuery = `SELECT CO2eqWtT,Cf_CO2_ggFuel,Cf_CO2eq_TtW, CO2eqTTW,CO2eqWtT, LCV FROM fuel_data WHERE Pathway_Name = ?`;
 
-          // TTW hesaplama işlemleri
-          db.query(fuelQuery, [ayak.sea_fuel], (err, seaFuelResult) => {
-            if (err || seaFuelResult.length === 0) {
-              console.error(
-                "Yakıt verisi alınırken hata:",
-                err ? err.message : "Yakıt bulunamadı"
-              );
-              return;
-            }
-            const seaCf_CO2_ggFuel = seaFuelResult[0].Cf_CO2_ggFuel;
-            const seaLCV = seaFuelResult[0].LCV;
-
-            db.query(fuelQuery, [ayak.port_fuel], (err, portFuelResult) => {
-              if (err || portFuelResult.length === 0) {
-                console.error(
-                  "Yakıt verisi alınırken hata:",
-                  err ? err.message : "Yakıt bulunamadı"
-                );
-                return;
-              }
-              const portCf_CO2_ggFuel = portFuelResult[0].Cf_CO2_ggFuel;
-              const portLCV = portFuelResult[0].LCV;
-
-              db.query(fuelQuery, [ayak.eca_fuel], (err, ecaFuelResult) => {
-                if (err || ecaFuelResult.length === 0) {
-                  console.error(
-                    "Yakıt verisi alınırken hata:",
-                    err ? err.message : "Yakıt bulunamadı"
-                  );
-                  return;
-                }
-                const ecaCf_CO2_ggFuel = ecaFuelResult[0].Cf_CO2_ggFuel;
-                const ecaLCV = ecaFuelResult[0].LCV;
-
-                // Diğer hesaplamalar...
-
-                const insertAyakQuery = `
-                  INSERT INTO ${tableName} (
-                      from_liman, to_liman, status, start_date, end_date, distance, distance_eca, port_day, speed, denizde_kalinan_sure, 
-                      gunluk_tuketim_sea, gunluk_tuketim_port, sea_fuel, eca_fuel, port_fuel, 
-                      sea_consumption, eca_consumption, port_consumption
-                  )
-                  VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-                `;
-
-                const ayakValues = [
-                  ayak.from,
-                  ayak.to,
-                  status,
-                  ayak.start_date,
-                  ayak.end_date,
-                  isNaN(ayak.distance) ? 0 : ayak.distance,
-                  isNaN(ayak.distance_eca) ? 0 : ayak.distance_eca,
-                  isNaN(ayak.port_day) ? 0 : ayak.port_day,
-                  isNaN(ayak.speed) ? 0 : ayak.speed,
-                  isNaN(ayak.denizde_kalinan_sure) ? 0 : ayak.denizde_kalinan_sure,
-                  isNaN(seferBilgisi.gunluk_tuketim_sea)
-                    ? 0
-                    : seferBilgisi.gunluk_tuketim_sea,
-                  isNaN(seferBilgisi.gunluk_tuketim_port)
-                    ? 0
-                    : seferBilgisi.gunluk_tuketim_port,
-                  ayak.sea_fuel,
-                  ayak.eca_fuel,
-                  ayak.port_fuel,
-                  isNaN(seaConsumption) ? 0 : seaConsumption,
-                  isNaN(ecaConsumption) ? 0 : ecaConsumption,
-                  isNaN(portConsumption) ? 0 : portConsumption,
-                ];
-
-                db.query(insertAyakQuery, ayakValues, (err) => {
-                  if (err) {
-                    console.error(
-                      `Ayak ${index + 1} bilgisi eklenirken hata oluştu:`,
-                      err.message
-                    );
+            // TTW hesaplama işlemleri
+            db.query(fuelQuery, [ayak.sea_fuel], (err, seaFuelResult) => {
+                if (err || seaFuelResult.length === 0) {
+                    console.error('Yakıt verisi alınırken hata:', err ? err.message : 'Yakıt bulunamadı');
                     return;
-                  }
-                  console.log(`Ayak ${index + 1} bilgisi başarıyla eklendi.`);
-                });
-              });
-            });
-          });
-        });
-      });
-    });
+                }
+                const seaCO2eqWtT= seaFuelResult[0].CO2eqWtT;
+                const seaCf_CO2_ggFuel = seaFuelResult[0].Cf_CO2_ggFuel;
+                const seaLCV = seaFuelResult[0].LCV;
+                const seaCO2eqTTW = seaFuelResult[0].CO2eqTTW;
+                const seaTotalConsumption = (consumption100Sea + consumption50Sea) * seaCf_CO2_ggFuel;
 
-    res.send("Sefer bilgileri başarıyla kaydedildi.");
-  });
+                db.query(fuelQuery, [ayak.port_fuel], (err, portFuelResult) => {
+                    if (err || portFuelResult.length === 0) {
+                        console.error('Yakıt verisi alınırken hata:', err ? err.message : 'Yakıt bulunamadı');
+                        return;
+                    }
+                    const portCf_CO2_ggFuel = portFuelResult[0].Cf_CO2_ggFuel;
+                    const portLCV = portFuelResult[0].LCV;
+                    const portCf_CO2eq_TtW = portFuelResult[0].Cf_CO2eq_TtW;
+                    const portCO2eqWtT= portFuelResult[0].CO2eqWtT;
+
+                    const portTotalConsumption = consumption100Port * portCf_CO2_ggFuel;
+
+                    db.query(fuelQuery, [ayak.eca_fuel], (err, ecaFuelResult) => {
+                        if (err || ecaFuelResult.length === 0) {
+                            console.error('Yakıt verisi alınırken hata:', err ? err.message : 'Yakıt bulunamadı');
+                            return;
+                        }
+                        const ecaCf_CO2_ggFuel = ecaFuelResult[0].Cf_CO2_ggFuel;
+                        const ecaLCV = ecaFuelResult[0].LCV;
+                        const ecaCf_CO2eq_TtW = ecaFuelResult[0].Cf_CO2eq_TtW;
+                        const ecaCO2eqWtT= ecaFuelResult[0].CO2eqWtT;
+
+                        const ecaTotalConsumption = (consumption100Eca + consumption50Eca) * ecaCf_CO2_ggFuel;
+
+                        // Tüm sonuçları topla ve 0,4 ile çarparak etsValue'yu hesapla
+                        const etsValue = 0.4 * (seaTotalConsumption + portTotalConsumption + ecaTotalConsumption);
+
+                        // Fuel Consumption Total hesapla
+                        const fuelConsumptionTotal = consumption100Sea + consumption50Sea + zeroSeaConsumption +
+                            consumption100Eca + consumption50Eca + zeroEcaConsumption +
+                            consumption100Port + consumption0Port;
+
+                        // TTW hesaplama: CO2eqTTW ve LCV kullanarak
+                        const ttwEmissions = ((consumption100Sea + consumption50Sea) * seaCf_CO2_ggFuel + (consumption100Eca + consumption50Eca) * ecaCf_CO2eq_TtW + consumption100Port * portCf_CO2eq_TtW) / ((consumption100Sea + consumption50Sea) * seaLCV + (consumption100Eca + consumption50Eca) * ecaLCV + consumption100Port * portLCV);
+                        // TTW hesaplama: CO2eqTTW ve LCV kullanarak
+                        const wtt=((consumption100Sea+consumption50Sea)*seaCO2eqWtT*seaLCV+consumption100Port*portCO2eqWtT*portLCV+(consumption100Eca+consumption50Eca)*ecaLCV*ecaCO2eqWtT)/((consumption100Sea+consumption50Sea)*seaLCV+consumption100Port*portLCV+(consumption100Eca+consumption50Eca*ecaLCV))
+                        const GHG_ACTUAL=(wtt+ttwEmissions)
+
+                        const COMPLIANCE_BALANCE = (89.34 - GHG_ACTUAL) * ((consumption100Sea + consumption50Sea) * seaLCV + (consumption50Eca + consumption100Eca) * ecaLCV + consumption100Port * portLCV) * 1000000;
+                        let fuel_eu;
+                        if (GHG_ACTUAL > 89.34) {
+                            fuel_eu = Math.abs(COMPLIANCE_BALANCE) / (GHG_ACTUAL * 41000) * 2400;
+                        } else {
+                            fuel_eu = 0;
+                        }
+                        // Veritabanına TTW emisyonları ekleme
+                        const insertAyakQuery = `
+                            INSERT INTO ${tableName} (
+                                from_liman, to_liman, status, distance, distance_eca, port_day, speed, denizde_kalinan_sure, 
+                                gunluk_tuketim_sea, gunluk_tuketim_port, sea_consumption, eca_consumption, port_consumption,
+                                consumption_100_sea, consumption_50_sea, consumption_100_eca, consumption_50_eca, consumption_100_port, consumption_0_port,
+                                sea_fuel, eca_fuel, port_fuel, zeroSeaConsumption, zeroEcaConsumption, ets, Fuel_Consumption_Total, TTW,wtt,GHG_ACTUAL,COMPLIANCE_BALANCE,fuel_eu
+                            )
+                            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,?,?,?,?)
+                        `;
+
+                        const ayakValues = [
+                            ayak.from, 
+                            ayak.to, 
+                            status,
+                             
+                            isNaN(ayak.distance) ? 0 : ayak.distance, 
+                            isNaN(ayak.distance_eca) ? 0 : ayak.distance_eca, 
+                            isNaN(ayak.port_day) ? 0 : ayak.port_day, 
+                            isNaN(ayak.speed) ? 0 : ayak.speed, 
+                            isNaN(ayak.denizde_kalinan_sure) ? 0 : ayak.denizde_kalinan_sure,
+                            isNaN(seferBilgisi.gunluk_tuketim_sea) ? 0 : seferBilgisi.gunluk_tuketim_sea, 
+                            isNaN(seferBilgisi.gunluk_tuketim_port) ? 0 : seferBilgisi.gunluk_tuketim_port,
+                            isNaN(seaConsumption) ? 0 : seaConsumption, 
+                            isNaN(ecaConsumption) ? 0 : ecaConsumption, 
+                            isNaN(portConsumption) ? 0 : portConsumption,
+                            isNaN(consumption100Sea) ? 0 : consumption100Sea, 
+                            isNaN(consumption50Sea) ? 0 : consumption50Sea, 
+                            isNaN(consumption100Eca) ? 0 : consumption100Eca, 
+                            isNaN(consumption50Eca) ? 0 : consumption50Eca, 
+                            isNaN(consumption100Port) ? 0 : consumption100Port, 
+                            isNaN(consumption0Port) ? 0 : consumption0Port,
+                            ayak.sea_fuel, 
+                            ayak.eca_fuel, 
+                            ayak.port_fuel, 
+                            isNaN(zeroSeaConsumption) ? 0 : zeroSeaConsumption, 
+                            isNaN(zeroEcaConsumption) ? 0 : zeroEcaConsumption, 
+                            isNaN(etsValue) ? 0 : etsValue, 
+                            isNaN(fuelConsumptionTotal) ? 0 : fuelConsumptionTotal, 
+                            isNaN(ttwEmissions) ? 0 : ttwEmissions,
+                            isNaN(wtt) ? 0: wtt,
+                            isNaN(GHG_ACTUAL) ? 0: GHG_ACTUAL,
+                            isNaN(COMPLIANCE_BALANCE) ? 0: COMPLIANCE_BALANCE,
+                            isNaN(fuel_eu) ? 0: fuel_eu
+
+                        ];
+
+                        db.query(insertAyakQuery, ayakValues, (err) => {
+                            if (err) {
+                                console.error(`Ayak ${index + 1} bilgisi eklenirken hata oluştu:`, err.message);
+                                return;
+                            }
+                            console.log(`Ayak ${index + 1} bilgisi başarıyla eklendi.`);
+                        });
+                    });
+                });
+            });
+        });
+    });
+});
+
+res.send('Sefer bilgileri başarıyla kaydedildi.');
+});
 });
 app.get("/sefer-fuel-toplami", (req, res) => {
   const query = `SHOW TABLES LIKE 'sefer%'`;
@@ -373,3 +404,142 @@ app.get("/sefer-fuel-toplami", (req, res) => {
     });
   });
 });
+app.get("/sefer-ets-toplami", (req, res) => {
+  const query = `SHOW TABLES LIKE 'sefer%'`;
+  
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error("Tablo sorgusu sırasında hata:", err.message);
+      return res.status(500).json({ error: "Veritabanı hatası oluştu." });
+    }
+
+    let totalFuel = 0;
+    let completed = 0;
+
+    results.forEach((row) => {
+      const tableName = Object.values(row)[0];
+      const fuelQuery = `SELECT IFNULL(SUM(ets), 0) AS fuel_sum FROM ${tableName}`;
+
+      db.query(fuelQuery, (err, fuelResults) => {
+        if (err) {
+          console.error(`Tablo ${tableName} için sorgu hatası:`, err.message);
+          return res.status(500).json({ error: "Fuel sorgusu sırasında hata oluştu." });
+        }
+
+        totalFuel += fuelResults[0].fuel_sum;
+        completed++;
+
+        // Tüm tablolar işlendiğinde sonucu döndür
+        if (completed === results.length) {
+          console.log(`Toplam ets: ${totalFuel}`);
+          res.json({ totalFuel });
+        }
+      });
+    });
+  });
+});
+app.get("/sefer-consumption-toplami", (req, res) => {
+  const query = `SHOW TABLES LIKE 'sefer%'`;  // sefer ile başlayan tabloları bul
+
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error("Tablo sorgusu sırasında hata:", err.message);
+      return res.status(500).json({ error: "Veritabanı hatası oluştu." });
+    }
+
+    let totalFuel = 0;
+    let completed = 0;
+
+    results.forEach((row) => {
+      const tableName = Object.values(row)[0];
+      // Üç sütunu toplayarak toplam tüketimi hesapla
+      const fuelQuery = `SELECT IFNULL(SUM(sea_consumption + port_consumption + eca_consumption), 0) AS fuel_sum FROM ${tableName}`;
+
+      db.query(fuelQuery, (err, fuelResults) => {
+        if (err) {
+          console.error(`Tablo ${tableName} için sorgu hatası:`, err.message);
+          return res.status(500).json({ error: "Fuel sorgusu sırasında hata oluştu." });
+        }
+
+        totalFuel += fuelResults[0].fuel_sum;
+        completed++;
+
+        // Tüm tablolar işlendiğinde sonucu döndür
+        if (completed === results.length) {
+          console.log(`Toplam consumption: ${totalFuel}`);
+          res.json({ totalFuel });
+        }
+      });
+    });
+  });
+});
+
+app.get("/sefer-balance-toplami", (req, res) => {
+  const query = `SHOW TABLES LIKE 'sefer%'`;
+  
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error("Tablo sorgusu sırasında hata:", err.message);
+      return res.status(500).json({ error: "Veritabanı hatası oluştu." });
+    }
+
+    let totalFuel = 0;
+    let completed = 0;
+
+    results.forEach((row) => {
+      const tableName = Object.values(row)[0];
+      const fuelQuery = `SELECT IFNULL(SUM(COMPLIANCE_BALANCE), 0) AS fuel_sum FROM ${tableName}`;
+
+      db.query(fuelQuery, (err, fuelResults) => {
+        if (err) {
+          console.error(`Tablo ${tableName} için sorgu hatası:`, err.message);
+          return res.status(500).json({ error: "Fuel sorgusu sırasında hata oluştu." });
+        }
+
+        totalFuel += fuelResults[0].fuel_sum;
+        completed++;
+
+        // Tüm tablolar işlendiğinde sonucu döndür
+        if (completed === results.length) {
+          console.log(`Toplam ets: ${totalFuel}`);
+          res.json({ totalFuel });
+        }
+      });
+    });
+  });
+});
+app.get("/sefer-ghg-toplami", (req, res) => {
+  const query = `SHOW TABLES LIKE 'sefer%'`;
+  
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error("Tablo sorgusu sırasında hata:", err.message);
+      return res.status(500).json({ error: "Veritabanı hatası oluştu." });
+    }
+
+    let totalFuel = 0;
+    let completed = 0;
+
+    results.forEach((row) => {
+      const tableName = Object.values(row)[0];
+      const fuelQuery = `SELECT IFNULL(SUM(89.34-	GHG_ACTUAL), 0) AS fuel_sum FROM ${tableName}`;
+
+      db.query(fuelQuery, (err, fuelResults) => {
+        if (err) {
+          console.error(`Tablo ${tableName} için sorgu hatası:`, err.message);
+          return res.status(500).json({ error: "Fuel sorgusu sırasında hata oluştu." });
+        }
+
+        totalFuel += fuelResults[0].fuel_sum;
+        completed++;
+
+        // Tüm tablolar işlendiğinde sonucu döndür
+        if (completed === results.length) {
+          console.log(`Toplam ets: ${totalFuel}`);
+          res.json({ totalFuel });
+        }
+      });
+    });
+  });
+});
+
